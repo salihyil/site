0d15d77b0e3faf9b50c58a1ea38517b4ea3322bf
salih <salih_yil@live.com>
2024-12-20 01:08:35 +0300

    year

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Salih | Personal Portfolio",
-  description: "Salih is a full-stack developer with 8 years of experience.",
+  description: "Salih is a full-stack developer with 3 years of experience.",
 };
 
 export default function RootLayout({
